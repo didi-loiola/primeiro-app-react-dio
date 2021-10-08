1b4f639eba6073f39432cab55e128e92854d9e0e
Diego Loiola <diego.alves_1996@hotmail.com>
2021-10-08 15:49:27 -0300

    Criando o primeiro componente

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 const App = () => {
   return (
-    <h1>Hello Wolrd</h1>
+    <>
+    <h1>Minha primeira aplicação com react</h1>
+    <ul>
+      
+      <li>Item 2</li>
+      <li>Item 3</li>
+    </ul>
+    </>
   )
 }
 
